11922873172f43683a96afc9db69c5d9d91a2909
roetezadi <roetezadi@gmail.com>
2021-11-28 16:38:04 +0330

    solve cpu error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dadmatools",
-    version="1.1.9",
+    version="1.1.10",
     author="Dadmatech AI Company",
     author_email="info@dadmatech.ir",
     description="DadmaTools is a Persian NLP toolkit",
