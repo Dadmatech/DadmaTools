45b3da61b54d5024ff7e1a506cf23a6dda562cb3
karrabi.mohammad <karrabi.mohammad@gmail.com>
2021-12-18 17:58:26 +0330

    add necessary imports to init datasets and filenames filed to info.json

---
+++ dadmatools/tests/datasets_test.py
@@ -0,0 +1,25 @@
+from dadmatools.datasets import ARMAN
+from dadmatools.datasets import get_datasets_info
+from dadmatools.datasets import TEP
+from dadmatools.datasets import PerSentLexicon
+from dadmatools.datasets import FaSpell
+from dadmatools.datasets import WikipediaCorpus
+if __name__ == '__main__':
+    print(get_datasets_info())
+    print(get_datasets_info(tasks=['NER']))
+    arman_dataset = ARMAN()
+    print(len(arman_dataset))
+    print(next(arman_dataset['train']))
+    print(next(arman_dataset['test']))
+    # tep_dataset = TEP()
+    # print(len(tep_dataset))
+    # print (next(tep_dataset))
+    # persent = PerSentLexicon()
+    # print(len(persent))
+    # print (next(persent))
+    # faspell = FaSpell()
+    # print(len(faspell))
+    # print (next(faspell['faspell_main']))
+    # wikipedia_corpus = WikipediaCorpus()
+    # print(len(wikipedia_corpus))
+    # print (next(wikipedia_corpus))
