3ac541e55028170b2d430464037bb6e628497e25
roetezadi <roetezadi@gmail.com>
2021-12-26 10:58:40 +0330

    update init

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dadmatools",
-    version="1.2.9",
+    version="1.3.0",
     author="Dadmatech AI Company",
     author_email="info@dadmatech.ir",
     description="DadmaTools is a Persian NLP toolkit",
