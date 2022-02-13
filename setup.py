55a8922fa2740853654af3599e831963e787b7ea
roetezadi <roetezadi@gmail.com>
2022-02-13 11:29:27 +0330

    address tokenizer

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dadmatools",
-    version="1.3.8",
+    version="1.3.9",
     author="Dadmatech AI Company",
     author_email="info@dadmatech.ir",
     description="DadmaTools is a Persian NLP toolkit",
