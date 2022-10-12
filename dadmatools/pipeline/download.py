204f1fe6aba465ff0cdc711cbaf81f92cbbc5b0d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 07:18:14 +0000

    Adding tarfile member sanitization to extractall()

--- dadmatools/pipeline/download.py
+++ dadmatools/pipeline/download.py
@@ -130,7 +130,26 @@ def _unzip_process_func(tmp_file_path: str, meta_info: dict, unzip_dir: str, cac
     print("Unziping the file {}".format(model_name + meta_info['file_extension']))
     
     with tarfile.open(full_path, mode='r:gz', compresslevel=9) as tar:
-        tar.extractall(path=unzip_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=unzip_dir)
             
             
 
