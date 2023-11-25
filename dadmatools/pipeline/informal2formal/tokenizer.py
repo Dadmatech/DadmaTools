eccd2d1db046b2b534a3892853ea9eb1c7d8edd4
Farhan Farsi <farhaaaaa1@gmail.com>
2023-11-25 13:24:26 +0330

    adding as utils to fix the 'utils' is not defined problem

--- dadmatools/pipeline/informal2formal/tokenizer.py
+++ dadmatools/pipeline/informal2formal/tokenizer.py
@@ -1,5 +1,6 @@
 import itertools
-import dadmatools.pipeline.informal2formal.utils
+import dadmatools.pipeline.informal2formal.utils as utils
+
 class InformalTokenizer:
     def __init__(self, vocab, postfixes):
         self.vocab = vocab
@@ -13,14 +14,13 @@ class InformalTokenizer:
     @staticmethod
     def get_postfixs(informal_postfix_addr):
         with open(informal_postfix_addr, 'r') as f:
-           ps = f.read().splitlines()
+            ps = f.read().splitlines()
         return ps
-
 
     def is_pre_post_word(self, w):
         nim_fasele = '‌'
         ws = w.split(nim_fasele)
-        pre, pos, v = [0,1,2]
+        pre, pos, v = [0, 1, 2]
         is_pre_pos = False
         state = pre
         valid_w = ''
@@ -61,12 +61,11 @@ class InformalTokenizer:
             return False
         return True
 
-
     def get_valid_word(self, words):
         seps = ['', '‌']
         all_seqs = []
         count = len(words)
-        lst = list(itertools.product(seps, repeat=count-1))
+        lst = list(itertools.product(seps, repeat=count - 1))
         for item in lst:
             seq = ''
             for word, sep in zip(words[:-1], item):
@@ -75,22 +74,22 @@ class InformalTokenizer:
             all_seqs.append(seq)
         return [w for w in all_seqs if w in self.vocab or self.is_pre_post_word(w)]
 
-    def get_candidates(self, tokens, index=0, current_seq = ' '):
+    def get_candidates(self, tokens, index=0, current_seq=' '):
         if index == len(tokens):
             return current_seq
         word = tokens[index]
         next_word, next_next_word = [None, None]
-        if index < len(tokens) -1:
-            next_word = tokens[index+1]
-        if index < len(tokens) -2:
-            next_next_word = tokens[index+2]
+        if index < len(tokens) - 1:
+            next_word = tokens[index + 1]
+        if index < len(tokens) - 2:
+            next_next_word = tokens[index + 2]
         cnds = []
         if next_word is not None:
             v_words = self.get_valid_word([word, next_word])
             if v_words:
                 for v_w in v_words:
                     current_seq1 = current_seq + ' ' + v_w
-                    cnds2 = self.get_candidates(tokens,index+2,  current_seq1)
+                    cnds2 = self.get_candidates(tokens, index + 2, current_seq1)
                     if type(cnds2) == str:
                         cnds.append(cnds2)
                     else:
@@ -100,25 +99,33 @@ class InformalTokenizer:
             if v_words:
                 for v_w in v_words:
                     current_seq2 = current_seq + ' ' + v_w
-                    cnds3 = self.get_candidates(tokens,index+3,  current_seq2)
+                    cnds3 = self.get_candidates(tokens, index + 3, current_seq2)
                     if type(cnds3) == str:
                         cnds.append(cnds3)
                     else:
                         cnds.extend(cnds3)
         current_seq = current_seq + ' ' + word
-        cnds1 = self.get_candidates(tokens,index+1,  current_seq)
+        cnds1 = self.get_candidates(tokens, index + 1, current_seq)
         if type(cnds1) == str:
             cnds.append(cnds1)
         else:
             cnds.extend(cnds1)
         return [c.strip() for c in cnds]
 
+    def if_connect(word1, word2):
+        not_connect_chars = ['ا', 'د', 'ذ', 'ر', 'ز', 'ژ', 'و']
+        if any(w == '' for w in [word1, word2]) or word1[-1] in not_connect_chars:
+            return True
+        return False
+
+
+
     def seperate_conjs(self, word, validator):
         conjs = ['و', 'در', 'با', 'تا', 'که', 'از', 'تو', 'من', 'شما']
         cnds = utils.split_conj_words(word, conjs)
         valid_cnds = [c for c in cnds if validator(c)]
         if valid_cnds:
-            return  valid_cnds
+            return valid_cnds
         return [word]
 
     def tokenize(self, txt, validator):
@@ -135,7 +142,7 @@ class InformalTokenizer:
         return txts
 
     def get_dense_tokens(self, tokens):
-        PRE, WORD, POST = 0,1,2
+        PRE, WORD, POST = 0, 1, 2
         out_tokens = []
         nim_fasele = '‌'
         current_word = ''
