ec74904edf6a25c0cb8d7ee8cba0503aea89a346
Sadegh Jafari <sadeghjafari5079@gmail.com>
2024-05-22 11:42:22 +0000

    fix emoji module version bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dadmatools",
-    version="2.0.2",
+    version="2.0.3",
     author="Dadmatech AI Company",
     author_email="info@dadmatech.ir",
     description="DadmaTools is a Persian NLP toolkit",
@@ -48,6 +48,7 @@ setuptools.setup(
         'sacremoses',
         'fasttext',
         'kenlm',
+        'emoji==2.10.0',
     ],
 
     dependency_links = [
