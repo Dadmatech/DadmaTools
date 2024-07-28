029e9c6c0f8f2a4072fc2ccf534e52fa7f33045c
Sadegh <sadeghjafari5079@gmail.com>
2024-07-28 10:17:00 +0330

    fix torch version for colab

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dadmatools",
-    version="2.0.6",
+    version="2.0.7",
     author="Dadmatech AI Company",
     author_email="info@dadmatech.ir",
     description="DadmaTools is a Persian NLP toolkit",
