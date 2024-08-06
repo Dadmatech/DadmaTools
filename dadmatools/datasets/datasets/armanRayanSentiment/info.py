aeeef93739a4f9d390b74c5ffd95eceba8f5bdce
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2024-08-06 18:27:35 +0330

    Revert "add arman rayan sharif datset"
    This reverts commit e03a8053e44ce7951fbbf5cf1ae934750117c42d.

--- dadmatools/datasets/datasets/armanRayanSentiment/info.py
+++
@@ -1,9 +0,0 @@
-dataset_info = {
-    "name": "ArmanTextEmotion",
-    "version": "1.0.0",
-    "task": "Text Emotion Classification",
-    "splits": ["train", "test", "dev"],
-    "description": "ArmanTextEmotion dataset contains text data labeled with different emotions. The dataset is used for training and evaluating emotion classification models.",
-    "filenames": ["train.txt", "test.txt", "dev.txt"],
-    "size": {"train": 13825, "test": 3456, "dev": 1536}  # Update these sizes with the actual sizes of your splits
-}
