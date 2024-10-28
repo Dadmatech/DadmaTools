a1a3d8017833021e826ef37b2437c1bbd1adf12a
sadeghjafari5528 <70170941+sadeghjafari5528@users.noreply.github.com>
2024-10-28 10:58:02 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dadmatools",
-    version="2.0.8",
+    version="2.0.9",
     author="Dadmatech AI Company",
     author_email="info@dadmatech.ir",
     description="DadmaTools is a Persian NLP toolkit",
